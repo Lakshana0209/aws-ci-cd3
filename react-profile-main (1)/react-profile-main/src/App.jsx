cafadd70865be22255fd33b3fb32a0bf85506b7c
Lakshana0209 <lakshana.s2024aids@sece.ac.in>
2026-05-12 15:16:43 +0530

    Update App.jsx

--- react-profile-main (1)/react-profile-main/src/App.jsx
+++ react-profile-main (1)/react-profile-main/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>laksha020906</h1>
+          <h1>lakshana</h1>
           <p>
           <code>CI CD</code>
           </p>
